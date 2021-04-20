aa1bb946c3f0a816567dfd7ee0e845c1fcfac2c8
AbhinavAKC <abhinav212001@gmail.com>
2021-04-20 15:07:07 +0530

    xyz

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <h1>Navbar Component</h1>
+            <div className='left'>
+                <a href="/"> Find Me A Mentor </a>
+            </div>
+            <div className='center'>
+                <input type="text" placeholder='Search' />
+            </div>
+
+            <div className='right'>
+                <a href="/"> <span>Home</span> </a>
+                <a href="./A    bout"> <span>About</span> </a>
+                <a href="/"> <span>Our Team</span> </a>
+                <Button onClick="/"> <span>Login</span> </Button>
+
+            </div>
         </div>
     )
 }
-
 export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,20 @@ module.exports = {
 			indigo: colors.indigo,
 			purple: colors.violet,
 			pink: colors.pink,
+			brgreen: {
+				DEFAULT: '#1DB954',
+				50: '#C6F6D7',
+				100: '#AFF3C7',
+				200: '#83ECA8',
+				300: '#57E589',
+				400: '#2BDE6A',
+				500: '#1DB954',
+				600: '#168D40',
+				700: '#0F612C',
+				800: '#083518',
+				900: '#010904'
+			  },
+			  
 		},
 		spacing: {
 			px: '1px',
