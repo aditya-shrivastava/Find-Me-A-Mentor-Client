0d9623f51dcabc063eb11b7f64913118ace0622b
riddhi26pal99 <palriddhi1999@gmail.com>
2021-04-21 12:06:59 +0530

    added header component

--- src/Components/FeaturedMentor/FeaturedMentor.jsx
+++ src/Components/FeaturedMentor/FeaturedMentor.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './FeaturedMentor.css';
+import RatingCard from '../FeaturedMentor/RatingCard/RatingCard'
+import SummaryCard from '../FeaturedMentor/SummaryCard/SummaryCard'
+import img3 from '../../Assets/Group 7.svg'
+
 
 const FeaturedMentor = () => {
     return (
-        <div className='featured-mentor'>
-            <h1>Featured Mentor Component</h1>
-        </div>
+        <React.Fragment>           
+            <container className='featured-mentor'>
+                <div className="rating">
+                    <RatingCard />
+                </div>
+                <div className="rating2">
+                    <RatingCard />
+                </div>
+                <div className="summary">
+                    <SummaryCard />
+                </div>
+                <img src={ img3 } alt= "" className="img3"/> 
+                <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="" width="400px" height = "400px" />
+            </container>
+        </React.Fragment>
+        
     )
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import './Header.css';
-import RatingCard from '../FeaturedMentor/RatingCard/RatingCard'
-import SummaryCard from '../FeaturedMentor/SummaryCard/SummaryCard'
+import FeaturedMentor from '../FeaturedMentor/FeaturedMentor'
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Button } from '@material-ui/core';
+import img1 from '../../Assets/Group 4.svg'
+import img2 from '../../Assets/Group 5.svg'
 
 
 const Header = () => {
     return (
-        <div className="header">
-        <RatingCard />
-        <SummaryCard />
-        </div>
+        <React.Fragment>
+            <div className="header">
+                <img src= {img2} alt="" className="img2"/>
+                <div className='Introduction'>
+                    <h1>
+                        Confused about your next move?
+                    </h1>
+                    <p>SignUp and get a personalized session with experts from your field</p>
+                        
+                    <div className="div">
+                        <span className="select">
+                        <select name="Select Category" id="categories">
+                        <option>Select Category</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                        </select>
+                        </span>
+                        
+                        <Button type= "submit">
+                        Find Mentor
+                        </Button>
+                    </div>  
+
+                    </div>
+                    
+                    <img src={img1} alt="" className="img1"/>  
+                
+                <FeaturedMentor />
+            </div>
+        </React.Fragment>
     )
 }
 
