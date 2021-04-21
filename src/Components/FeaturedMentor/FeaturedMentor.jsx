f645bc3c6631388b88f23503c4a57f87ebfa20a7
riddhi26pal99 <palriddhi1999@gmail.com>
2021-04-21 12:11:42 +0530

    edited the img dimension

--- src/Components/FeaturedMentor/FeaturedMentor.jsx
+++ src/Components/FeaturedMentor/FeaturedMentor.jsx
@@ -19,7 +19,7 @@ const FeaturedMentor = () => {
                     <SummaryCard />
                 </div>
                 <img src={ img3 } alt= "" className="img3"/> 
-                <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="" width="400px" height = "400px" />
+                <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="" width="400" height = "400" />
             </container>
         </React.Fragment>
         
