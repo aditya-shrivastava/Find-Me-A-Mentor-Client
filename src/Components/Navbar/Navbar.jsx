6255a05c81a33c99e420763411b674b4756fd5b3
AbhinavAKC <abhinav212001@gmail.com>
2021-04-22 12:21:50 +0530

    navbar for for not logged in

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -9,15 +10,14 @@ const Navbar = () => {
                 <a href="/"> Find Me A Mentor </a>
             </div>
             <div className='center'>
-                <input type="text" placeholder='Search' />
+                <TextField style={{ backgroundColor: '#C4C4C4', borderRadius: 5, width: 600}} InputProps={{ disableUnderline: true }} placeholder='Search'  />
             </div>
-
+            
             <div className='right'>
                 <a href="/"> <span>Home</span> </a>
-                <a href="./A    bout"> <span>About</span> </a>
-                <a href="/"> <span>Our Team</span> </a>
-                <Button onClick="/"> <span>Login</span> </Button>
-
+                <a href="/about"> <span>About</span> </a>
+                <a href="/team"> <span>Our Team</span> </a>
+                <Button style={{backgroundColor: 'white', color: '#1DB954'}} onClick="/"> <span>Login</span> </Button>
             </div>
         </div>
     )
