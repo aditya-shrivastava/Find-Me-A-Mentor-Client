1c92c9857edb774f9258e1db8520078ad89da755
AbhinavAKC <abhinav212001@gmail.com>
2021-05-24 21:01:17 +0530

    update profile modal added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { About, Header, Testimonials,} from '../Components';
 import TestimonialForm from '../Components/Testimonial_Form/TestimonialForm';
-import UpdateProfile from '../Components/DashBoard/UpdateProfile/UpdateProfile';
 
 const Home = () => {
     return (
         <div className='home'>
-            <UpdateProfile />
             <Header />
             <Testimonials />
             <About />
